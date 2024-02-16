3ff601aae6cc2abea9c22ff67e32e907241f1a58
Ivan Thung <ivanthung@Ivans-MacBook-Pro.local>
2024-02-16 09:49:30 +0100

    added nieuwbouw sliders

--- pages/2_2 - Nieuwbouw.py
+++ pages/2_2 - Nieuwbouw.py
@@ -7,6 +7,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import utils.layout as layout
+from streamlit_extras.add_vertical_space import add_vertical_space
 
 layout.set_page_title("Keuze nieuwbouw")
 session = st.session_state
@@ -15,21 +16,19 @@ session = st.session_state
 building_need_projection = {"Woningen": 600}
 overige_needs_projection_m2 = {"Kantoren": 10000, "Horeca": 5000, "Utiliteits": 3000}
 woning_typologie_m2 = {"Groot": 120, "Klein": 60, "Medium": 80}
-gebouwprofielen = {
+building_profiles = {
     "Mixed_used_toren": {
         "split": {"Woningen": 0.6, "Kantoren": 0.1, "Horeca": 0.1},
-        "impact_m2": 500,
+        "impact_m2": {"hybrid": 300, "secondary": 100, "regular":500},
+        "min_m2": 500,
     },
-    "Kantoorgebouw": {"split": {"Kantoren": 1}, "impact_m2": 500},
-    "Laagbouw": {"split": {"Woningen": 1}, "impact_m2": 500},
-    "Hoogbouw": {"split": {"Woningen": 1}, "impact_m2": 300},
+    "Kantoorgebouw": {"split": {"Kantoren": 1}, "impact_m2": {"hybrid": 100, "secondary": 200, "regular":500}, "min_m2": 1000},
+    "Laagbouw": {"split": {"Woningen": 1}, "impact_m2": {"hybrid": 100, "secondary": 200, "regular":500},  "min_m2": 120},
+    "Multi-family": {"split": {"Woningen": 1}, "impact_m2": {"hybrid": 60, "secondary": 30, "regular":100},  "min_m2": 480},
 }
 
-
-
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns((3,1,3))
 with col1:
-    st.write("Totaal te realiseren woningen:", building_need_projection["Woningen"])
     st.write("Kies percentage per woningtypologie.")
 
     slider = {
@@ -43,13 +42,12 @@ with col1:
     }
     totaal = sum(slider.values())
 
+    if (totaal) > 100:
+        st.warning(f"Totaal percentage: {totaal}% is groter dan 100%.")
+    else:
+        st.success(f"Totaal percentage: {totaal}%")
 
-if (totaal) > 100:
-    st.warning(f"Totaal percentage: {totaal}% is groter dan 100%.")
-else:
-    st.success(f"Totaal percentage: {totaal}%")
-
-with col2:
+with col3:
     m2_totaal = list(slider.values()) * np.array(list(woning_typologie_m2.values()))
     chart_data = pd.DataFrame(((m2_totaal),), columns=slider.keys())
 
@@ -57,15 +55,43 @@ with col2:
     st.bar_chart(chart_data)
 
 # Metrics of the projected needs
-st.write("### Woningen per typologie")
-columns = st.columns(len(slider))
-for i, (key, value) in enumerate(slider.items()):
-    with columns[i]:
+    
+    columns = st.columns(len(slider))
+    for i, (key, value) in enumerate(slider.items()):
+        with columns[i]:
+            woningen = int(value / 100 * building_need_projection["Woningen"])
+            label = f"{key} ({woning_typologie_m2[key]} m2)"
+            st.metric(
+                value=woningen,
+                label=label,
+                delta=f"{value}%",
+            )
 
-        woningen = int(value / 100 * building_need_projection["Woningen"])
-        label = f"{key} ({woning_typologie_m2[key]} m2)"
-        st.metric(
-            value=woningen,
-            label=label,
-            delta=f"{value}%",
+add_vertical_space(2)
+st.divider()
+st.write("### Gebouwprofielen")
+
+if "building_profile" not in session:
+    session.building_profile = {}  # Initialize as a list
+
+col1, col2, col3 = st.columns((1,1,1))
+
+bu_ty = list(building_profiles.keys())
+se_ty = unique_impact_keys = {key for profile in building_profiles.values() for key in profile['impact_m2'].keys()}
+
+building_types = col2.selectbox("Gebouwtype", bu_ty)
+secondary_type = col3.selectbox("Profiel", se_ty)
+
+if (col1.button("Create")):
+    session.building_profile[building_types] = { secondary_type : 0 }
+
+col1, col2, col3 = st.columns((3,1,3))
+for p in session.building_profile.keys():
+    with col1:
+        st.slider(
+            key = p,
+            min_value=0,
+            max_value=100,
+            label=p,
+            value=0,
         )
